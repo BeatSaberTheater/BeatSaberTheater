0860c0713a915276bce5b74c9eea50865e68f930
Willem <willem@wisp.software>
2025-10-11 02:25:30 +0200

    feat(reactive-settings-ui): convert Level Detail component to reactive

---
+++ BeatSaberTheater/Util/ReactiveUi/BackgroundExtensions.cs
@@ -0,0 +1,17 @@
+using HMUI;
+using Reactive.BeatSaber.Components;
+using Reactive.Components;
+using UnityEngine;
+
+namespace BeatSaberTheater.Util.ReactiveUi;
+
+public static class BackgroundExtensions
+{
+	public static T AsBeatSaberBackground<T>(
+		this T holder)
+		where T : IComponentHolder<Image>
+	{
+		return holder.AsBackground(color: new Color(0, 0, 0), gradientColor0: new Color(255, 255, 255, 0.75f), gradientColor1: new Color(255, 255, 255, 0.5f),
+			gradientDirection: ImageView.GradientDirection.Horizontal, type: UnityEngine.UI.Image.Type.Sliced);
+	}
+}
---
+++ BeatSaberTheater/VideoMenu/LevelDetailComponent.cs
@@ -0,0 +1,73 @@
+using System;
+using BeatSaberTheater.Util.ReactiveUi;
+using Reactive;
+using Reactive.BeatSaber.Components;
+using Reactive.Yoga;
+using UnityEngine;
+
+namespace BeatSaberTheater.VideoMenu;
+
+public class LevelDetailComponent(Transform parent) : ReactiveComponent
+{
+	public event Action? ButtonPressed;
+	private Label _label = null!;
+	private BsButton _button = null!;
+
+	public void SetLabelText(string text, Color color, bool active)
+	{
+		_label.Text = text;
+		_label.Enabled = active;
+		_label.Color = color.ColorWithAlpha(0.75f);
+	}
+
+	public void SetButtonState(string text, Color color, bool active)
+	{
+		_button.Interactable = active;
+		_button._underline.Color = color;
+		_button.Enabled = active;
+		_button.Text = text;
+	}
+
+	protected override GameObject Construct()
+	{
+		return new Background()
+			{
+				WithinLayoutIfDisabled = true,
+				LayoutModifier = new YogaModifier()
+				{
+					Margin = new YogaFrame()
+					{
+						left = 2.pt(),
+						right = 2.pt()
+					}
+				},
+				Children =
+				{
+					new Label()
+						{
+							WithinLayoutIfDisabled = false,
+							Text = "Video available",
+							Color = Color.white.ColorWithAlpha(0.75f)
+						}
+						.AsFlexItem(flex: 0)
+						.Bind(ref _label),
+					
+					new BsButton()
+						{
+							WithinLayoutIfDisabled = false,
+							Text = "Download",
+							OnClick = () =>
+							{
+								Plugin._log.Info("Downloading video...");
+								ButtonPressed?.Invoke();
+							}
+						}
+						.AsFlexItem(0)
+						.Bind(ref _button)
+				}
+			}
+			.AsFlexGroup(FlexDirection.Row, Justify.SpaceAround, constrainVertical: false, padding: new YogaFrame(0, YogaValue.Point(2)))
+			.AsBeatSaberBackground()
+			.Use(parent);
+	}
+}
